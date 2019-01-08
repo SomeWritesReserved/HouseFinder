bd18be62c16efaea170b219fa223c2c87f16753b
SomeWritesReserved <SomeWritesReserved@users.noreply.github.com>
2019-01-07 21:00:30 -0500

    House finder with scraped OBX and SB houses

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace HouseFinderUI
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.scrapeObxButton = new System.Windows.Forms.Button();
+			this.dumpDebugButton = new System.Windows.Forms.Button();
+			this.parseObxButton = new System.Windows.Forms.Button();
+			this.dataGridView1 = new System.Windows.Forms.DataGridView();
+			this.dataGridView2 = new System.Windows.Forms.DataGridView();
+			this.scrapeSbButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(130, 110);
+			this.webBrowser1.TabIndex = 0;
+			this.webBrowser1.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webBrowser1_DocumentCompleted);
+			// 
+			// scrapeObxButton
+			// 
+			this.scrapeObxButton.Enabled = false;
+			this.scrapeObxButton.Location = new System.Drawing.Point(12, 185);
+			this.scrapeObxButton.Name = "scrapeObxButton";
+			this.scrapeObxButton.Size = new System.Drawing.Size(75, 23);
+			this.scrapeObxButton.TabIndex = 1;
+			this.scrapeObxButton.Text = "Scrape OBX";
+			this.scrapeObxButton.UseVisualStyleBackColor = true;
+			this.scrapeObxButton.Click += new System.EventHandler(this.scrapeObxButton_Click);
+			// 
+			// dumpDebugButton
+			// 
+			this.dumpDebugButton.Location = new System.Drawing.Point(12, 116);
+			this.dumpDebugButton.Name = "dumpDebugButton";
+			this.dumpDebugButton.Size = new System.Drawing.Size(75, 23);
+			this.dumpDebugButton.TabIndex = 2;
+			this.dumpDebugButton.Text = "Dump";
+			this.dumpDebugButton.UseVisualStyleBackColor = true;
+			this.dumpDebugButton.Click += new System.EventHandler(this.dumpDebugButton_Click);
+			// 
+			// parseObxButton
+			// 
+			this.parseObxButton.Location = new System.Drawing.Point(12, 225);
+			this.parseObxButton.Name = "parseObxButton";
+			this.parseObxButton.Size = new System.Drawing.Size(75, 23);
+			this.parseObxButton.TabIndex = 3;
+			this.parseObxButton.Text = "Parse OBX";
+			this.parseObxButton.UseVisualStyleBackColor = true;
+			this.parseObxButton.Click += new System.EventHandler(this.parseObxButton_Click);
+			// 
+			// dataGridView1
+			// 
+			this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView1.Location = new System.Drawing.Point(379, 0);
+			this.dataGridView1.Name = "dataGridView1";
+			this.dataGridView1.Size = new System.Drawing.Size(1189, 511);
+			this.dataGridView1.TabIndex = 4;
+			// 
+			// dataGridView2
+			// 
+			this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridView2.Location = new System.Drawing.Point(12, 254);
+			this.dataGridView2.Name = "dataGridView2";
+			this.dataGridView2.Size = new System.Drawing.Size(361, 257);
+			this.dataGridView2.TabIndex = 5;
+			// 
+			// scrapeSbButton
+			// 
+			this.scrapeSbButton.Enabled = false;
+			this.scrapeSbButton.Location = new System.Drawing.Point(130, 185);
+			this.scrapeSbButton.Name = "scrapeSbButton";
+			this.scrapeSbButton.Size = new System.Drawing.Size(75, 23);
+			this.scrapeSbButton.TabIndex = 6;
+			this.scrapeSbButton.Text = "Scrape SB";
+			this.scrapeSbButton.UseVisualStyleBackColor = true;
+			this.scrapeSbButton.Click += new System.EventHandler(this.scrapeSbButton_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1580, 518);
+			this.Controls.Add(this.scrapeSbButton);
+			this.Controls.Add(this.dataGridView2);
+			this.Controls.Add(this.dataGridView1);
+			this.Controls.Add(this.parseObxButton);
+			this.Controls.Add(this.dumpDebugButton);
+			this.Controls.Add(this.scrapeObxButton);
+			this.Controls.Add(this.webBrowser1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.WebBrowser webBrowser1;
+		private System.Windows.Forms.Button scrapeObxButton;
+		private System.Windows.Forms.Button dumpDebugButton;
+		private System.Windows.Forms.Button parseObxButton;
+		private System.Windows.Forms.DataGridView dataGridView1;
+		private System.Windows.Forms.DataGridView dataGridView2;
+		private System.Windows.Forms.Button scrapeSbButton;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Device.Location;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace HouseFinderUI
+{
+	public partial class Form1 : Form
+	{
+		private string currentHouse;
+		private AutoResetEvent autoResetEvent = new AutoResetEvent(false);
+		private int docCompletedCount = 0;
+		private int houseCompletedCount = 0;
+		private volatile bool isWaiting = false;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+		{
+			if (this.isWaiting)
+			{
+				this.docCompletedCount++;
+				this.autoResetEvent.Set();
+				this.isWaiting = false;
+			}
+		}
+
+		private void dumpDebugButton_Click(object sender, EventArgs e)
+		{
+			var documentAsIHtmlDocument3 = (mshtml.IHTMLDocument3)this.webBrowser1.Document.DomDocument;
+			var content = documentAsIHtmlDocument3.documentElement.innerHTML;
+
+			File.WriteAllText("test.html", content);
+		}
+
+
+		private void scrapeObxButton_Click(object sender, EventArgs e)
+		{
+			new Thread(() =>
+			{
+				// Step 1: Scrape all obx houses
+				XmlDocument xmlDocument = new XmlDocument();
+				xmlDocument.Load(@"obxhouses.xml");
+
+				var allHouses = xmlDocument.SelectNodes("houses/div/div/a").OfType<XmlNode>().Select((node) => node.Attributes["href"].Value).ToArray();
+
+				foreach (var housePath in allHouses)
+				{
+					string fullUrl = "https://www.outerbeaches.com" + housePath;
+
+					this.currentHouse = housePath;
+					Directory.CreateDirectory("obx");
+
+					this.Invoke(new Action(() =>
+					{
+						this.Text = $"({this.houseCompletedCount} - ${this.docCompletedCount}) {housePath}";
+						this.isWaiting = true;
+						this.webBrowser1.Navigate(fullUrl);
+					}));
+					this.autoResetEvent.WaitOne();
+					this.autoResetEvent.Reset();
+					Thread.Sleep(1000);
+
+					this.Invoke(new Action(() =>
+					{
+						var documentAsIHtmlDocument3 = (mshtml.IHTMLDocument3)this.webBrowser1.Document.DomDocument;
+						var content = documentAsIHtmlDocument3.documentElement.innerHTML;
+						File.WriteAllText(Path.Combine("obx", this.currentHouse.Replace("/property/", string.Empty)) + ".html", content);
+					}));
+					this.houseCompletedCount++;
+				}
+			}).Start();
+		}
+
+
+		private void scrapeSbButton_Click(object sender, EventArgs e)
+		{
+			new Thread(() =>
+			{
+				// Step 1: Scrape all obx houses
+				XmlDocument xmlDocument = new XmlDocument();
+				xmlDocument.Load(@"sbhouses.xml");
+
+				var allHouses = xmlDocument.SelectNodes("ul/li").OfType<XmlNode>()
+					.Select((node) => node.SelectSingleNode(".//a").Attributes["href"].Value)
+					.Select((fullUrl) => fullUrl.Substring(0, fullUrl.IndexOf('?')))
+					.ToArray();
+
+				foreach (var housePath in allHouses)
+				{
+					string fullUrl = "https://www.sandbridge.com" + housePath;
+
+					WebClient webClient = new WebClient();
+					webClient.DownloadFile(fullUrl, Path.Combine("sb", housePath.Replace("/rental/house/", string.Empty)) + ".html");
+
+					/*this.currentHouse = housePath;
+					Directory.CreateDirectory("sb");
+
+					this.Invoke(new Action(() =>
+					{
+						this.Text = $"({this.houseCompletedCount} - ${this.docCompletedCount}) {housePath}";
+						this.isWaiting = true;
+						this.webBrowser1.Navigate(fullUrl);
+					}));
+					this.autoResetEvent.WaitOne();
+					this.autoResetEvent.Reset();
+					Thread.Sleep(1000);
+
+					this.Invoke(new Action(() =>
+					{
+						var documentAsIHtmlDocument3 = (mshtml.IHTMLDocument3)this.webBrowser1.Document.DomDocument;
+						var content = documentAsIHtmlDocument3.documentElement.innerHTML;
+						File.WriteAllText(Path.Combine("sb", this.currentHouse.Replace("/rental/house/", string.Empty)) + ".html", content);
+					}));
+					this.houseCompletedCount++;*/
+				}
+			}).Start();
+		}
+
+		private void parseObxButton_Click(object sender, EventArgs e)
+		{
+			// Step 2: Parse scraped houses for information
+			List<Rental> rentals = new List<Rental>();
+			foreach (string houseFile in Directory.GetFiles("obx"))
+			{
+				string houseText = File.ReadAllText(houseFile);
+				string name = Regex.Match(houseText, "<h1 class=\"property-page-title\">(.+)</h1>").Groups[1].Value;
+				string summary = Regex.Match(houseText, "<h2 class=\"property-page-subtitle\">(.+)</h2>").Groups[1].Value;
+				if (summary.Contains("Condo")) { continue; }
+
+				Rental rental = new Rental();
+				rental.Name = name;
+				rental.Bedrooms = int.Parse(Regex.Match(summary, @"(\d+) [bB]edroom").Groups[1].Value);
+				rental.Town = Regex.Match(summary, @"in (.+)").Groups[1].Value;
+				rental.RentalType = (RentalType)Enum.Parse(typeof(RentalType), Regex.Match(summary, @",(.+) [hH]ouse").Groups[1].Value.Trim().Replace(" ", "").Replace("-", ""), true);
+				rental.HasPool = houseText.Contains("lr-021-pool");
+
+				var coordinateMatch = Regex.Match(houseText, @"map.setView\(\[(-?\d+\.\d+) , (-?\d+\.\d+)\]");
+				rental.CoordinateX = coordinateMatch.Groups[1].Value;
+				rental.CoordinateY = coordinateMatch.Groups[2].Value;
+
+				foreach (var match in Regex.Matches(houseText, "<tr.+?</tr>").OfType<Match>().Skip(1))
+				{
+					XmlDocument weeklyPriceXmlDoc = new XmlDocument();
+					weeklyPriceXmlDoc.LoadXml(match.Value);
+					string weekStart = weeklyPriceXmlDoc.FirstChild.ChildNodes[1].InnerText;
+					string priceString = weeklyPriceXmlDoc.FirstChild.ChildNodes[3].InnerText;
+					if (priceString == "Reserved") { continue; }
+					int price = int.Parse(priceString.Replace("$", "").Replace(",", ""));
+
+					rental.WeekPrices.Add(new WeekPrice()
+					{
+						StartDate = DateTime.Parse(weekStart.Trim().Replace("th,", ",").Replace("st,", ",").Replace("nd,", ",").Replace("rd,", ",")),
+						Price = price,
+					});
+				}
+
+				rentals.Add(rental);
+			}
+
+			//================================
+			// filter
+			//================================
+			rentals = rentals
+				.Where((r) => r.RentalType == RentalType.Oceanfront || r.RentalType == RentalType.SemiOceanfront || r.RentalType == RentalType.Oceanside)
+				//.Where((r) => r.Bedrooms == 4 || r.Bedrooms == 5)
+				//.Where((r) => r.HasPool)
+				//.Where((r) => r.PricePerBedroom < 810)
+				.Where((r) => r.DatePrice > 0)
+				.Where((r) => r.Town != "Frisco" && r.Town != "Buxton" && r.Town != "Hatteras")
+				.ToList();
+
+			//================================
+			// calc distances
+			//================================
+			List<BetweenDistance> betweenDistances = new List<BetweenDistance>();
+			foreach (Rental rentalA in rentals)
+			{
+				Rental closestRental = null;
+				double closestDistance = double.MaxValue;
+				foreach (Rental rentalB in rentals)
+				{
+					if (rentalA == rentalB) { continue; }
+
+					GeoCoordinate coordinateA = new GeoCoordinate(double.Parse(rentalA.CoordinateX), double.Parse(rentalA.CoordinateY));
+					GeoCoordinate coordinateB = new GeoCoordinate(double.Parse(rentalB.CoordinateX), double.Parse(rentalB.CoordinateY));
+					double distanceBetween = Math.Round(coordinateA.GetDistanceTo(coordinateB) * 3.28084);
+
+					if (distanceBetween < closestDistance)
+					{
+						closestRental = rentalB;
+						closestDistance = distanceBetween;
+					}
+
+					betweenDistances.Add(new BetweenDistance()
+					{
+						DistanceBetween = distanceBetween,
+						RentalA = rentalA,
+						RentalB = rentalB,
+					});
+				}
+				rentalA.ClosestRental = closestRental;
+				rentalA.DistanceTo = closestDistance;
+			}
+
+
+			this.Text = rentals.Count.ToString();
+			this.dataGridView1.DataSource = rentals
+				.OrderBy((r) => r.DistanceTo)
+				//.OrderBy((r) => r.Town)
+				//.ThenBy((r) => r.PricePerBedroom)
+				.ToList();
+
+			this.dataGridView2.DataSource = betweenDistances
+				.OrderBy((d) => d.DistanceBetween)
+				.Where((d) => d.DistanceBetween < 1320)
+				.ToList();
+		}
+	}
+
+	public class Rental
+	{
+		public List<WeekPrice> WeekPrices = new List<WeekPrice>();
+		public string Name { get; set; }
+		public RentalType RentalType { get; set; }
+		public string Town { get; set; }
+		public int Bedrooms { get; set; }
+		public bool HasPool { get; set; }
+
+		public int DatePrice
+		{
+			get
+			{
+				int price = -1;
+
+				var weeks = this.WeekPrices.Where((w) => w.StartDate.Month == 8 && (w.StartDate.Day == 9 || w.StartDate.Day == 10));
+				if (weeks.Any())
+				{
+					price = weeks.Single().Price;
+				}
+
+				return price;
+			}
+		}
+
+		public int PricePerBedroom
+		{
+			get { return this.DatePrice / this.Bedrooms; }
+		}
+
+		public Rental ClosestRental { get; set; }
+		public double DistanceTo { get; set; }
+
+		public string CoordinateX { get; set; }
+		public string CoordinateY { get; set; }
+
+
+		public override string ToString()
+		{
+			return this.Name;
+		}
+	}
+
+	public class WeekPrice
+	{
+		public DateTime StartDate { get; set; }
+		public int Price { get; set; }
+	}
+
+	public class BetweenDistance
+	{
+		public double DistanceBetween { get; set; }
+		public string RentalAName { get { return this.RentalA.Name; } }
+		public string RentalBName { get { return this.RentalB.Name; } }
+		public Rental RentalA;
+		public Rental RentalB;
+	}
+
+	public enum RentalType
+	{
+		Oceanfront,
+		SemiOceanfront,
+		Oceanside,
+
+		Soundfront,
+		SemiSoundfront,
+		Soundside,
+
+		Canalfront,
+	}
+}
